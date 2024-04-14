73fb1aa6c6fb6480aad391727d5bc60a62032a77
gvroque17 <gvargasr@andrew.cmu.edu>
2024-04-14 16:09:47 +0000

    adding utils.py, __init__, LICENSE, setup.py, ping_site_test.py, utils.py

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 # 06-643-Project
 
 Will list the most cited papers for a search to help pinpoint fundamental papers
--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 
 from setuptools import setup
 
-setup(name='ReferencedWorkSort',
+setup(name='ReferencedWorkSortPkg',
       version='0.0.1',
       description='Sorts referenced work by first author',
       maintainer='Graciela Vargas Roque',
       maintainer_email='gvargasr@andrew.cmu.edu',
       license='MIT',
-      packages=['ReferencedWorkSort'],
-      entry_points={'console_scripts': ['referenced_work_sort=ReferencedWorkSort.utils:referenced_work_sort']},  
+      packages=['ReferencedWorkSortPkg'],
+      entry_points={'console_scripts': ['referenced_work_sort=ReferencedWorkSortPkg.utils:referenced_work_sort']},  
       scripts=[],
       long_description='''For a paper OID, lists referenced work grouped by author
 =========================''')
