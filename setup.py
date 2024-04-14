b8ca883dd09aa624c4db26831e8f992f070b54ad
gvroque17 <gvargasr@andrew.cmu.edu>
2024-04-14 16:20:39 +0000

    adding utils.py, __init__, LICENSE, setup.py, ping_site_test.py, utils.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 
 from setuptools import setup
 
-setup(name='ReferencedWorkSortPkg',
+setup(name='RefWorkSortPkg',
       version='0.0.1',
       description='Sorts referenced work by first author',
       maintainer='Graciela Vargas Roque',
       maintainer_email='gvargasr@andrew.cmu.edu',
       license='MIT',
-      packages=['ReferencedWorkSortPkg'],
-      entry_points={'console_scripts': ['referenced_work_sort=ReferencedWorkSortPkg.utils:referenced_work_sort']},  
+      packages=['RefWorkSortPkg'],
+      entry_points={'console_scripts': ['referenced_work_sort=RefWorkSortPkg.utils:referenced_work_sort']},  
       scripts=[],
       long_description='''For a paper OID, lists referenced work grouped by author
 =========================''')
