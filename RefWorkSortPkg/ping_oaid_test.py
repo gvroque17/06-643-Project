5f23e98f7918f5dc3f1f2442d2904aa92eb3945f
gvroque17 <gvargasr@andrew.cmu.edu>
2024-04-15 18:07:01 +0000

    updating to fix errors in tests for utils.py, setup.py, ping_site_test.py, utils.py

--- RefWorkSortPkg/ping_oaid_test.py
+++ RefWorkSortPkg/ping_oaid_test.py
@@ -4,13 +4,10 @@ import requests
 import pytest
 
 
-@pytest.fixture(
-    scope="module",
-    params=[
-        "https://doi.org/10.1021/acscatal.5b00538",  # Good OAID
-        "https://doi.org/10.021/acscatal.5b00538",  # Bad OAID
-    ],
-)
+@pytest.fixture(scope="module", params=[
+    'https://doi.org/10.1021/acscatal.5b00538',  # Good OAID
+    'https://doi.org/10.021/acscatal.5b00538'   # Bad OAID
+])
 def oaid(request):
     """Fixture to pass in oaid for testing oaid."""
     return request.param
@@ -21,14 +18,14 @@ def test_ping_site(oaid):
     # User inputs only the OAID of the paper of interest
     # Acessing only OAID receives a status 403: access forbidden
     # Concatenate OAID to use entire URL to check website is working
-    url = "https://api.openalex.org/works/" + oaid
+    url = 'https://api.openalex.org/works/' + oaid
     try:
         # requests website server response and gets returns status code
         req = requests.get(url)
         statuscode = req.status_code
 
         # Use returned status code to check for success(numberes in 2xx)
-        if statuscode >= 200 and statuscode < 300:
+        if (statuscode >= 200 and statuscode < 300):
             status = "URL found and working properly."
         assert status == "URL found and working properly."
 
--- RefWorkSortPkg/ref_work_test.py
+++ RefWorkSortPkg/ref_work_test.py
@@ -3,27 +3,19 @@
 # import requests
 import pytest
 from RefWorkSortPkg import utils
-
 # from RefWorkSortPkg import referenced_work_sort
 
 
-@pytest.fixture(
-    scope="module",
-    params=[
-        ("https://doi.org/10.48550/arxiv.2306.10055", {}),  # no references OAID
-        (
-            "https://doi.org/10.1016/0002-9378(87)90010-x",
-            {
-                "Douglas A. Triplett": [
-                    "Douglas A. Triplett, Hemostasis: A Case Oriented Approach, (1985), None. cited by: 1. https://openalex.org/W606611693"
-                ],
-                "John H. Griffin": [
-                    "Bernhard Lämmle and John H. Griffin, Formation of the Fibrin Clot: the Balance of Procoagulant and Inhibitory Factors, (1985), https://doi.org/10.1016/s0308-2261(21)00478-1. cited by: 50. https://openalex.org/W2411065424"
-                ],
-            },
-        ),  # 2 references OAID
-    ],
-)
+@pytest.fixture(scope="module", params=[
+    ('https://doi.org/10.48550/arxiv.2306.10055',
+     {}),  # no references OAID
+    ('https://doi.org/10.1016/0002-9378(87)90010-x',
+     {'Douglas A. Triplett':
+      ['Douglas A. Triplett, Hemostasis: A Case Oriented Approach, (1985), None. cited by: 1. https://openalex.org/W606611693'],
+      'John H. Griffin':
+      ['Bernhard Lämmle and John H. Griffin, Formation of the Fibrin Clot: the Balance of Procoagulant and Inhibitory Factors, (1985), https://doi.org/10.1016/s0308-2261(21)00478-1. cited by: 50. https://openalex.org/W2411065424']}
+     )  # 2 references OAID
+])
 def oaid(request):
     """Fixture to pass oaid for testing."""
     return request.param
